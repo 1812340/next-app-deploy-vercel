fb0769197663058b1015317d56121fe1138f129b
waqas-ghori <waqasghori467@gmail.com>
2026-03-20 00:22:34 +0100

    Automatic workflow CI/CD

--- app/page.tsx
+++ app/page.tsx
@@ -17,8 +17,8 @@ export default function Home() {
             Learning jenkins with github and deploying the application on vercle 
           </h1>
           <p className="max-w-md text-lg leading-8 text-zinc-600 dark:text-zinc-400">
-            Looking for a starting point or more instructions? Head over to{" "}
-            <a
+              Github + jenkins + vercel is a powerful combination for modern web development. 
+              <a
               href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
               className="font-medium text-zinc-950 dark:text-zinc-50"
             >
